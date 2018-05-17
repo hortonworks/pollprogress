0838aebe0622ac9dd4c29338f1b56c1641a49f0b
Krisztian Horvath <keyki.kk@gmail.com>
2018-05-17 13:25:48 +0200

    remove multibar

--- main.go
+++ main.go
@@ -12,8 +12,6 @@ import (
 	"time"
 	"math"
 
-	"github.com/sethgrid/multibar"
-
 	"errors"
 	"gopkg.in/yaml.v2"
 )
@@ -34,7 +32,7 @@ func poll(cmd string) (int, int, error) {
 		return 0, 0, errors.New(fmt.Sprintf("Cannot convert actual copy progress to int: %s", parts[0]))
 	}
 
-	secondPart := strings.Split(parts[1],"\n")
+	secondPart := strings.Split(parts[1], "\n")
 
 	sum, err := strconv.Atoi(strings.TrimSpace(secondPart[0]))
 	if err != nil {
@@ -43,11 +41,11 @@ func poll(cmd string) (int, int, error) {
 
 	status := strings.TrimSpace(secondPart[1])
 	switch status {
-	    case
-	        "pending",
-					"success":
-	        return act, sum, nil
-	    }
+	case
+		"pending",
+		"success":
+		return act, sum, nil
+	}
 	return 0, 0, errors.New(fmt.Sprintf("Copy operation status is invalid: %s", status))
 }
 
@@ -61,57 +59,47 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't read file: %v", err)
 	}
-	var obj map[string]string
-	err = yaml.Unmarshal([]byte(data), &obj)
+	var tasks map[string]string
+	err = yaml.Unmarshal([]byte(data), &tasks)
 
-	progressBars, _ := multibar.New()
-	progressBars.Println("Azure blob copy progress:")
+	log.Println("Azure blob copy progress tracking started")
 
 	wg := &sync.WaitGroup{}
-	wg.Add(len(obj))
+	wg.Add(len(tasks))
 
 	totals := map[string]int{}
 	log.Println("Getting blob sizes..")
-	for task, cmd := range obj {
-		go func(task, cmd string) {
+	for storageAccount, cmd := range tasks {
+		go func(storageAccount, cmd string) {
 			defer wg.Done()
 			_, sum, err := poll(cmd)
 			for err != nil {
 				time.Sleep(time.Second * 1)
-				log.Printf("Error finding the total size of the vhd in Storage Account: %s, error: %s", task, err.Error())
+				log.Printf("Error finding the total size of the vhd in Storage Account: %s, error: %s", storageAccount, err.Error())
 				_, sum, err = poll(cmd)
 			}
-			log.Printf("Blob size of vhd in Storage Account of %s is: %f GB", task, float64(sum) / math.Pow(1024,3))
-			totals[task] = sum
-		}(task, cmd)
+			log.Printf("Blob size of vhd in Storage Account of %s is: %f GB", storageAccount, float64(sum)/math.Pow(1024, 3))
+			totals[storageAccount] = sum
+		}(storageAccount, cmd)
 	}
 	wg.Wait()
 
-	wg.Add(len(obj))
-	for task, cmd := range obj {
-		p := progressBars.MakeBar(totals[task], fmt.Sprintf("%-30s", task))
-		go func(cmd string, progressFn multibar.ProgressFunc) {
+	wg.Add(len(tasks))
+	for storageAccount, cmd := range tasks {
+		go func(storageAccount, cmd string) {
 			defer wg.Done()
 			act, sum, err := poll(cmd)
 			if err == nil {
-				progressFn(act)
+				log.Printf("Copy status to Storage Account of %s is: (%d/%d) %.2f%% ", storageAccount, act, sum, (float64(sum)/float64(act))*100)
 			}
 			for act < sum || err != nil {
-				time.Sleep(time.Second * 1)
-				act, _, err = poll(cmd)
+				time.Sleep(time.Second * 10)
+				act, sum, err = poll(cmd)
 				if err == nil {
-					progressFn(act)
+					log.Printf("Copy status to Storage Account of %s is: (%d/%d) %.2f%% ", storageAccount, act, sum, (float64(sum)/float64(act))*100)
 				}
 			}
-			progressFn(act)
-
-		}(cmd, p)
-	}
-
-	go progressBars.Listen()
-
-	for _, b := range progressBars.Bars {
-		b.Update(0)
+		}(storageAccount, cmd)
 	}
 
 	wg.Wait()
